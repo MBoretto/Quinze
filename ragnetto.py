992b9db201d2b622423c445414d7fe09b6bcfe90
alienmaster <alienmaster@mag07xl.to.infn.it>
2014-01-28 16:57:03 +0100

    Inizio del Feedback i risultati ottenuti dalla classe sono uniti a quelli iniziali, è necessrio implementare un filtro nella classe che elimini gli elementi con < << soli numeri e cose incomprensibili tipo 7&578//

--- ragnetto.py
+++ ragnetto.py
@@ -1,6 +1,59 @@
 #!/usr/bin/python
 
 import httplib
+import HTMLParser
+from urlparse import urlsplit, urlparse, parse_qs
+
+######################
+#Classe che trova i link ad altre stazioni
+######################
+#ispirata da http://docs.python.org/2/library/htmlparser.html
+# http://stackoverflow.com/questions/3276040/how-can-i-use-the-python-htmlparser-library-to-extract-data-from-a-specific-div
+
+# create a subclass and override the handler methods
+class Id_stazioni(HTMLParser.HTMLParser):
+	def __init__(self):
+		HTMLParser.HTMLParser.__init__(self)
+		self.F_a = 0
+		self.Id = 0			
+		self.stazioni = dict()
+
+	def handle_starttag(self, tag, attrs):
+		if tag == 'a':			
+			#print attrs
+			#print len(attrs)
+			for i in range(len(attrs)):
+				if attrs[i][0] == 'href':
+					linkStruct = urlsplit(attrs[i][1])
+					if linkStruct.path == 'QO_Arrivi_SiPMR.aspx':
+						self.F_a = 1						
+						explode_query = parse_qs(urlparse(attrs[i][1]).query)
+						self.Id = explode_query['Id'][0]
+						#print self.Id						
+
+
+	def handle_endtag(self, tag):
+		if tag == 'a':
+			self.F_a = 0
+			#print				
+
+	def handle_data(self, data):
+		if self.F_a == 1:			
+			data = data.strip(' \t\n\r')
+			#bisogna inserire un controllo che mi filtra i link con i numeri e i caratteri delle pagine
+			self.stazioni[str(self.Id)] = data			
+			#print self.stazioni[str(self.Id)]
+						
+
+	def get_result(self):
+        	return self.stazioni
+
+
+############################################
+
+
+stazioni= dict()
+stazioni['2672']= 'Torino+Porta+Nuova'
 
 conn = httplib.HTTPSConnection('prm.rfi.it',443)
 #scommentare per debug
@@ -25,56 +78,23 @@ conn.request('GET','/qo_prm/QO_Arrivi_SiPMR.aspx'
 	)
 
 response = conn.getresponse()
-print response.status, response.reason
 
-data = response.read()
-conn.close()
-#print data
+if response.status == 200 :
+	data = response.read()
+	conn.close()
+	#print data
 
-#preso da http://docs.python.org/2/library/htmlparser.html
-# http://stackoverflow.com/questions/3276040/how-can-i-use-the-python-htmlparser-library-to-extract-data-from-a-specific-div
-
-
-import HTMLParser
-from urlparse import urlsplit, urlparse, parse_qs
-
-
-# create a subclass and override the handler methods
-class MyHTMLParser(HTMLParser.HTMLParser):
-	def __init__(self):
-		HTMLParser.HTMLParser.__init__(self)
-		self.F_a = 0		
-
-	def handle_starttag(self, tag, attrs):
-		if tag == 'a':			
-			#print attrs
-			#print len(attrs)
-			for i in range(len(attrs)):
-				if attrs[i][0] == 'href':
-					linkStruct = urlsplit(attrs[i][1])
-					if linkStruct.path == 'QO_Arrivi_SiPMR.aspx':
-						#print 'OOOK!'
-						self.F_a = 1
-						#print linkStruct.query
-						explode_query = parse_qs(urlparse(attrs[i][1]).query)
-						print explode_query['Id'][0]
-						#print attrs[i][0], ' ' , attrs[i][1]
-
-			
+	# instantiate the parser and fed it some HTML
+	parser = Id_stazioni()
+	parser.feed(data)
+	stazioni_nuove = parser.get_result()
+	#print stazioni_nuove
+	#chiavi_stazioni = stazioni.keys()
+	#print chiavi_stazioni
+	stazioni.update(stazioni_nuove)
+	print stazioni
 	
+else:
+	print 'Errore: ', response.status, response.reason
 
-	def handle_endtag(self, tag):
-		if tag == 'a':
-			self.F_a = 0
-			#print
-		
 
-	def handle_data(self, data):
-		if self.F_a == 1:
-			data = data.strip(' \t\n\r')
-			print data 
-			print
-
-# instantiate the parser and fed it some HTML
-parser = MyHTMLParser()
-parser.feed(data)
