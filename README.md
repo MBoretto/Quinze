7acc6920dabb856d2cc92861fd90fc984690ce64
mconcas <mett1990@gmail.com>
2014-01-28 23:05:55 +0100

    cornicette, a me muore al 17 interact.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-Quinze
-======
+The Quinze Project
+==================
 
-La cosa per fare i rifugi nella neve.
+«La cosa per fare i rifugi nella neve.»
+
+The Quinze project is the nth brainchild of Marco Boretto.
+His main purpose is already undefined. 
--- ragnetto.py
+++ ragnetto.py
@@ -4,13 +4,16 @@ import httplib
 import HTMLParser
 from urlparse import urlsplit, urlparse, parse_qs
 
-######################
-#Classe che trova i link ad altre stazioni
-######################
-#ispirata da http://docs.python.org/2/library/htmlparser.html
-# http://stackoverflow.com/questions/3276040/how-can-i-use-the-python-htmlparser-library-to-extract-data-from-a-specific-div
-
+######################################################################
+# Classe che trova i link ad altre stazioni
+#
+# inspired by: http://docs.python.org/2/library/htmlparser.html
+# http://stackoverflow.com/questions/3276040/how-can-i-use-the-python-
+# htmlparser-library-to-extract-data-from-a-specific-div
+######################################################################
+#
 # create a subclass and override the handler methods
+
 class Id_stazioni(HTMLParser.HTMLParser):
 	def __init__(self):
 		HTMLParser.HTMLParser.__init__(self)
@@ -27,7 +30,8 @@ class Id_stazioni(HTMLParser.HTMLParser):
 					linkStruct = urlsplit(attrs[i][1])
 					if linkStruct.path == 'QO_Arrivi_SiPMR.aspx':
 						self.F_a = 1						
-						explode_query = parse_qs(urlparse(attrs[i][1]).query)
+						explode_query = parse_qs(
+							urlparse(attrs[i][1]).query)
 						self.Id = explode_query['Id'][0]
 						#print self.Id						
 
@@ -35,12 +39,14 @@ class Id_stazioni(HTMLParser.HTMLParser):
 	def handle_endtag(self, tag):
 		if tag == 'a':
 			self.F_a = 0
-			#print				
 
 	def handle_data(self, data):
 		if self.F_a == 1:			
 			data = data.strip(' \t\n\r')
-			#bisogna inserire un controllo che mi filtra i link con i numeri e i caratteri delle pagine
+			#############################################################
+			# bisogna inserire un controllo che mi filtra i link con i 
+			# numeri e i caratteri delle pagine.
+			#############################################################
 			self.stazioni[str(self.Id)] = data			
 			#print self.stazioni[str(self.Id)]
 						
@@ -49,7 +55,7 @@ class Id_stazioni(HTMLParser.HTMLParser):
         	return self.stazioni
 
 
-############################################
+######################################################################
 
 
 stazioni= dict()
@@ -77,12 +83,9 @@ while i < 30 :
 		+ '&Id=' + chiavi_stazioni[i]
 		+ '&pag=' + '01'
 		+ '&start=' + '0'
-
 		+ '&stop=' + '6'
-		+ '&partenzeAddrVal=' + 
-		'http%3A%2F%2Fprm.rfi.it%2Fqo_prm%2FQO_Partenze_SiPMR.aspx'
-
-		)
+		+ '&partenzeAddrVal='  
+		+ 'http%3A%2F%2Fprm.rfi.it%2Fqo_prm%2FQO_Partenze_SiPMR.aspx')
 
 	response = conn.getresponse()
 
