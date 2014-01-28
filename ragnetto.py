9b90d6d9265ba8405c1b838ae3a191a746fa0ad9
MBoretto <marco.bore@gmail.com>
2014-01-28 17:40:39 +0100

    feedback funzionante si rompe dopo un po di interazioni

--- ragnetto.py
+++ ragnetto.py
@@ -55,46 +55,57 @@ class Id_stazioni(HTMLParser.HTMLParser):
 stazioni= dict()
 stazioni['2672']= 'Torino+Porta+Nuova'
 
-conn = httplib.HTTPSConnection('prm.rfi.it',443)
-#scommentare per debug
-#conn.set_debuglevel(1)
-
-conn.request('GET','/qo_prm/QO_Arrivi_SiPMR.aspx'
-	+ '?'
-	+ 'car1=' + 'arrivi'
-	+ '&dalle=' + '04.00'
-	+ '&alle=' + '08.00'
-	+ '&txtStazione=' + 'Torino+Porta+Nuova'
-	+ '&x=' + '39'
-	+ '&y=' + '11'
-	+ '&Id=' + '2672'
-	+ '&pag=' + '01'
-	+ '&start=' + '0'
-
-	+ '&stop=' + '6'
-	+ '&partenzeAddrVal=' + 
-	'http%3A%2F%2Fprm.rfi.it%2Fqo_prm%2FQO_Partenze_SiPMR.aspx'
-
-	)
-
-response = conn.getresponse()
-
-if response.status == 200 :
-	data = response.read()
-	conn.close()
-	#print data
-
-	# instantiate the parser and fed it some HTML
-	parser = Id_stazioni()
-	parser.feed(data)
-	stazioni_nuove = parser.get_result()
-	#print stazioni_nuove
-	#chiavi_stazioni = stazioni.keys()
-	#print chiavi_stazioni
-	stazioni.update(stazioni_nuove)
-	print stazioni
-	
-else:
-	print 'Errore: ', response.status, response.reason
 
 
+i = 0
+while i < 30 :
+
+	chiavi_stazioni = stazioni.keys()
+
+	conn = httplib.HTTPSConnection('prm.rfi.it',443)
+	#scommentare per debug
+	#conn.set_debuglevel(1)
+	print 'interazione' , i
+	conn.request('GET','/qo_prm/QO_Arrivi_SiPMR.aspx'
+		+ '?'
+		+ 'car1=' + 'arrivi'
+		+ '&dalle=' + '04.00'
+		+ '&alle=' + '08.00'
+		+ '&txtStazione=' + stazioni[chiavi_stazioni[i]]
+		+ '&x=' + '39'
+		+ '&y=' + '11'
+		+ '&Id=' + chiavi_stazioni[i]
+		+ '&pag=' + '01'
+		+ '&start=' + '0'
+
+		+ '&stop=' + '6'
+		+ '&partenzeAddrVal=' + 
+		'http%3A%2F%2Fprm.rfi.it%2Fqo_prm%2FQO_Partenze_SiPMR.aspx'
+
+		)
+
+	response = conn.getresponse()
+
+	if response.status == 200 :
+		data = response.read()
+		conn.close()
+		#print data
+
+		# instantiate the parser and fed it some HTML
+		parser = Id_stazioni()
+		parser.feed(data)
+		stazioni_nuove = parser.get_result()
+		#del stazioni_nuove[chiavi_stazioni[0]]
+		#print stazioni_nuove
+		#chiavi_stazioni = stazioni.keys()
+		#print chiavi_stazioni
+		stazioni.update(stazioni_nuove)
+		print stazioni
+		print
+
+	else:
+		print 'Errore: ', response.status, response.reason
+		#print  chiavi_stazioni[i] , stazioni[chiavi_stazioni[i]]
+
+	i = i + 1
+		
