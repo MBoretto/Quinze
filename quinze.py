8efd3416b4c9c7d5a87582131c20af05af03deb8
MBoretto <MBoretto@users.noreply.github.com>
2014-01-30 00:58:34 +0100

    Update quinze.py

--- quinze.py
+++ quinze.py
@@ -1,2 +1,6 @@
 #perchè salvare i dati in file di testo quando subito si possono sparare in un db?
+#Mysql non c'è su aulaa 
 #inspire http://zetcode.com/db/mysqlpython/
+#sqlite 
+#http://www.sqlite.org/whentouse.html
+#http://zetcode.com/db/sqlitepythontutorial/
